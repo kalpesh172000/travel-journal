bc688df6f83c186dd0b700e33c65b473f6000727
kalpesh <kalpesh200071@gmail.com>
2025-01-14 11:10:50 +0000

    initial commit

---
+++ src/components/header.jsx
@@ -0,0 +1,11 @@
+import PublicIcon from "@mui/icons-material/Public";
+export default function Header() {
+    return (
+        <>
+            <header>
+                <PublicIcon />
+                <span className="title">my travel journal.</span>
+            </header>
+        </>
+    );
+}
